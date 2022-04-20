bd77f7944a886291134f115599cf41ecc356ea3e
Peter <peter.rangelov@myacuity.com>
2022-04-20 07:14:21 -0400

    More leetcoding

---
+++ src/test/java/medium/Pr200NumIslandsTest.java
@@ -0,0 +1,47 @@
+package medium;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class Pr200NumIslandsTest {
+
+  private Pr200NumIslands pr;
+
+  private static Stream<Arguments> provideData() {
+    return Stream.of(
+      Arguments.of(
+        new char[][] {
+          {'1', '1', '1', '1', '0'},
+          {'1', '1', '0', '1', '0'},
+          {'1', '1', '0', '0', '0'},
+          {'0', '0', '0', '0', '0'}
+        }, 1),
+      Arguments.of(
+        new char[][] {
+          {'1', '1', '0', '0', '0'},
+          {'1', '1', '0', '0', '0'},
+          {'0', '0', '1', '0', '0'},
+          {'0', '0', '0', '1', '1'}
+        }, 3)
+
+    );
+
+  }
+
+  @BeforeEach
+  void setup() {
+    pr = new Pr200NumIslands();
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideData")
+  void testAll(char[][] grid, int count) {
+    Assertions.assertEquals(count, pr.numIslands(grid));
+  }
+
+}
